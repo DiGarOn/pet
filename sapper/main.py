99b1b52f7ab2786ee66d48bdd3aa0d58d988e613
Dmitriy Garkin <Dmitriy.Garkin@infowatch.com>
2023-04-09 23:02:12 +0300

    ready sapper

--- sapper/main.py
+++ sapper/main.py
@@ -3,8 +3,8 @@ import tkinter.ttk as ttk
 import random
 from functools import partial
 
-class MainWindow:
 
+class MainWindow:
     def __init__(self):
         self.window = Tk()
 
@@ -20,7 +20,6 @@ class MainWindow:
         self.combo['values'] = ('8x8', '10x10', '12x12', '16x16', '24x24', '32x32')
         self.combo.current(0)
         self.combo.grid(column=1, row=0)
-        # self.combo.bind("<<ComboboxSelected>>", self.fullfill_data)
 
         self.button = Button(self.window, text='start the game', command=self.Start)
         self.button.grid(column=2, row=0)
@@ -29,24 +28,21 @@ class MainWindow:
 
         self.buttoms = []
 
-    def open_1(self, i, j, flag):
-        pass
-        # for k in range(i, len(self.data), 1):
-        #     if self.data[k][j] == -1:
-        #         return
-        #     elif self.data[k][j] != 0:
-        #         self.buttoms[k][j].configure(text=self.data[k][j])
-        #         return
-        #     else:
-        #         self.buttoms[k][j].configure(text=self.data[k][j])
-        #         for l in range(j+1, len(self.data), 1):
-        #             if self.data[k][l] == 0:
-        #                 self.buttoms[k][l].configure(text=self.data[k][l])
-        #                 if k + 1 < len(self.data):
-        #                     self.open_1(k+1, l, True)
-        #             else:
-        #                 self.buttoms[k][l].configure(text=self.data[k][l])
-        #                 break
+    def open_1(self, i, j):
+        # exit
+        if i < 0 or i == len(self.data) or j < 0 or j == len(self.data):
+            return
+        elif self.buttoms[i][j].cget('text') == self.data[i][j]:
+            return
+        elif self.data[i][j] > 0:
+            self.buttoms[i][j].configure(text=self.data[i][j])
+            return
+        else:
+            self.buttoms[i][j].configure(text=self.data[i][j])
+            self.open_1(i + 1, j)
+            self.open_1(i, j + 1)
+            self.open_1(i - 1, j)
+            self.open_1(i, j - 1)
 
     def pushed(self, i, j):
         if self.data[i][j] == -1:
@@ -54,18 +50,7 @@ class MainWindow:
             lbl = Label(end_game_window, text="Game Over.").pack()
             self.buttoms[i][j].configure(text=self.data[i][j])
         else:
-            self.buttoms[i][j].configure(text=self.data[i][j])
-            if self.data [i][j] == 0:
-                self.open_1(i,j, True)
-                # self.open_2(i, j, True)
-            # if i > 0:
-            #     self.open(i-1, j, 2, True)
-            # if j > 0:
-            #     self.open(i, j-1, 1, True)
-            # if i + 1 < len(self.data):
-            #     self.open(i+1, j, 3, True)
-            # if j + 1 < len(self.data):
-            #     self.open(i, j+1, 4, True)
+            self.open_1(i, j)
 
     def Start(self):
         self.buttoms = []
@@ -104,12 +89,10 @@ class MainWindow:
                     tmp.append(count)
 
             self.data.append(tmp)
-        for i in self.data:
-            print(i)
 
     def run(self):
         self.window.mainloop()
 
 
-window  = MainWindow()
+window = MainWindow()
 window.run()
